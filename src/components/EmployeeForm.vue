692510503ff875aca43734f76c85bc4810e284df
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-08-12 15:30:33 +0530

    Added basic validation checks for Employee Form

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -2,9 +2,27 @@
     <div id="employee-form">
         <form @submit.prevent="handleSubmit">
             <label>Employee Name</label>
-            <input v-model="employee.name" type="text" />
+            <input 
+                ref="first"
+                type="text"
+                :class="{ 'has-error': submitting && invalidName }"
+                v-model="employee.name"
+                @focus="clearStatus"
+                @keypress="clearStatus" 
+            />
             <label>Employee Email</label>
-            <input v-model="employee.email" type="text" />
+            <input 
+                type="text"
+                :class="{ 'has-error': submitting && invalidEmail }"
+                v-model="employee.email"
+                @focus="clearStatus"
+            />
+            <p v-if="error && submitting" class="error-message">
+                ❗Please fill out all required fields
+            </p>
+            <p v-if="success" class="success-message">
+                ✅ Employee successfully added
+            </p>
             <button>Add Employee</button>
         </form>
     </div>
@@ -15,6 +33,9 @@ export default {
     name: 'employee-form',
     data() {
         return{
+            submitting: false,
+            error: false,
+            success: false,
             employee: {
                 name: '',
                 email: '',
@@ -23,7 +44,36 @@ export default {
     },
     methods: {
         handleSubmit() {
+            this.submitting = true
+            this.clearStatus()
+
+            if(this.invalidName || this.invalidEmail){
+                this.error = true
+                return
+            }
+
             this.$emit('add:employee', this.employee)
+            this.$refs.first.focus()
+            this.employee = {
+                name: '',
+                email: '',
+            }
+            this.error = false
+            this.success = true
+            this.submitting = false
+        },
+
+        clearStatus() {
+            this.success = false
+            this.error = false
+        },
+    },
+    computed: {
+        invalidName() {
+            return this.employee.name === ''
+        },
+        invalidEmail() {
+            return this.employee.email === ''
         },
     },
 }
@@ -32,5 +82,17 @@ export default {
 <style scoped>
     form {
         margin-bottom: 2rem;
+    }
+
+    [class*='-message'] {
+        font-weight: 500;
+    }
+
+    .error-message {
+        color:#d33c40;
+    }
+
+    .success-message {
+        color: #32a95d;
     }
 </style>
