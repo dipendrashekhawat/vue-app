58656431b4835aed85bae316dcb498cc45353fc7
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-08-16 15:28:51 +0530

    updated addEmployee() to async method and added async/await

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,18 @@ export default {
     }
   },
   methods: {
-    addEmployee(employee) {
-      const lastId = this.employees.length > 0 ? this.employees[this.employees.length - 1].id : 0;
-      const id = lastId + 1;
-      const newEmployee = { ...employee, id };
-    
-      this.employees = [...this.employees, newEmployee]
+    async addEmployee(employee) {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+          method: 'POST',
+          body: JSON.stringify(employee),
+          headers: { "Content-type": "application/json; charset=UTF-8" }
+        })
+        const data = await response.json()
+        this.employees = [...this.employees, data]
+      } catch (error) {
+        console.error('Error occured while adding employee: ' +error);
+      }
     },
 
     async getEmployees() {
