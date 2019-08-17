231c37a436339d514567eeafadab97f2e59e8792
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-08-17 20:38:11 +0530

    added action buttons for Edit and Delete functionalities

--- src/components/EmployeeDetails.vue
+++ src/components/EmployeeDetails.vue
@@ -1,10 +1,45 @@
 <template>
   <div id="employee-details">
-    <table>
+    <p v-if="employees.length < 1">
+      No employees
+    </p>
+    <table v-else>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
       <tbody>
-        <tr v-for="employee in employees" :key="employee.id">
-          <td>{{ employee.name }}</td>
+        <tr v-for="employee in employees" v-bind:key="employee.id">
+          <td v-if="editing === employee.id">
+            <input
+              type="text"
+              v-model="employee.name"
+            >
+          </td>
+          <td v-else>{{ employee.name }}</td>
+
+          <td v-if="editing === employee.id">
+            <input
+              type="text"
+              v-model="employee.email"
+            >
+          </td>
           <td>{{ employee.email }}</td>
+
+          <td v-if="editing === employee.id">
+            <button @click="editEmployee(employee)">Save</button>
+            <button
+              class="muted-button"
+              @click="cancelEdit(employee)"
+            >Cancel</button>
+          </td>
+          <td v-else>
+            <button @click="editMode(employee)">Edit</button>
+            <button @click="$emit('delete:employee', employee.id)">Delete</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -17,6 +52,19 @@
     props: {
       employees: Array,
     },
+
+    data() {
+      return {
+        editing: null,
+      }
+    },
+
+    methods: {
+      editMode(employee) {
+        this.cachedEmployee
+
+      },
+    }
   }
 </script>
 
